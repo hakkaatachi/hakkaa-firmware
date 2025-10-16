a6d1eb04e3387ca41c35f1355f61f062e662d0fa
Kiyotoko <karl.zschiebsch@gmail.com>
2025-10-16 23:24:06 +0200

    fix: remove qemu

--- flake.nix
+++ flake.nix
@@ -4,57 +4,25 @@
 {
   description = "hakkaa firmware flake";
   inputs = {
-    qemu-espressif.url = "github:SFrijters/nix-qemu-espressif";
-    nixpkgs.follows = "qemu-espressif/nixpkgs";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "qemu-espressif/nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, qemu-espressif, rust-overlay, ... }:
-    let
-      inherit (nixpkgs) lib;
-      flatAttrs = [ "overlays" "nixosModules" ];
-      # Inject a system attribute if the attribute is not one of the above
-      injectSystem = system:
-        lib.mapAttrs (name: value:
-          if lib.elem name flatAttrs then value else { ${system} = value; });
-      # Combine the above for a list of 'systems'
-      forSystems = systems: f:
-        lib.attrsets.foldlAttrs (acc: system: value:
-          lib.attrsets.recursiveUpdate acc (injectSystem system value)) { }
-        (lib.genAttrs systems f);
-
-      # Maybe other systems work as well, but they have not been tested
-    in forSystems [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ] (system:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) (import qemu-espressif) ];
+          overlays = [ (import rust-overlay) ];
         };
         toolchain =
           (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
-        name = "hackaa-firmware";
-        qemu-esp32c3 = pkgs.qemu-esp32c3;
       in {
-        packages = { inherit qemu-esp32c3; };
         devShells.default = pkgs.mkShellNoCC {
-          name = "${name}-dev";
+          name = "hakkaa-firmware";
 
-          packages = [
-            pkgs.espflash
-            pkgs.esptool
-            pkgs.gnugrep
-            pkgs.netcat
-            qemu-esp32c3
-            toolchain
-          ];
+          packages = [ pkgs.espflash pkgs.esptool toolchain ];
         };
         formatter = pkgs.nixfmt-tree;
       });
